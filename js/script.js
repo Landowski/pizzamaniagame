2ef7f8df431e165fa642a5315b170b52aac26c9c
Landowski <105506160+Landowski@users.noreply.github.com>
2025-04-28 13:28:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -188,7 +188,7 @@ document.addEventListener('DOMContentLoaded', function () {
     { clicks: 94000, reached: false },
     { clicks: 95000, reached: false },
     { clicks: 96000, reached: false },
-    { clicks: 970000, reached: false },
+    { clicks: 97000, reached: false },
     { clicks: 98000, reached: false },
     { clicks: 99000, reached: false },
     { clicks: 105000, reached: false },
@@ -289,7 +289,7 @@ document.addEventListener('DOMContentLoaded', function () {
   let lastClickTime = 0;
   const STREAK_REQUIREMENTS = [ 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200, 205, 210, 215, 220, 225, 230, 235, 240, 245, 250, 255, 260, 265, 270, 275, 280, 285, 290, 295, 300, 305, 310, 315, 320, 325, 330, 335, 340, 345, 350 ];
   let currentStreakLevel = 0;
-  const STREAK_TIMEOUT = 1800;
+  const STREAK_TIMEOUT = 1200;
   const translations = {
     'en': {
       buttonSellPizzas: 'SELL ALL PIZZAS!',
